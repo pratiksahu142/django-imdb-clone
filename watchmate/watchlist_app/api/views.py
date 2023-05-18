1a9253c088d88d6fb492a63f5394069226fc96ac
pratiksahu142 <pratik.sahu1421996@gmail.com>
2023-05-18 16:42:31 -0400

    Added class based views for review model

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 
 # from rest_framework.decorators import api_view
 from rest_framework.decorators import APIView
-from rest_framework import status, generics, mixins
+from rest_framework import status, generics
 from watchlist_app.models import WatchList, StreamPlatform, Review
 from watchlist_app.api.serializers import (
     WatchListSerializer,
@@ -11,25 +11,36 @@ from watchlist_app.api.serializers import (
 )
 
 
-class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+# Class Based views
+class ReviewList(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-
-class ReviewList(
-    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
-):
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+# class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+
+# class ReviewList(
+#     mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+# ):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
 class StreamPlatformAV(APIView):
