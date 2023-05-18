2d85a2b2c1fdb4b99451e5b37a8a74ef28a51976
pratiksahu142 <pratik.sahu1421996@gmail.com>
2023-05-18 14:44:05 -0400

    Added GenericAPIView and mixins to review model for create and retrieve

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -6,6 +6,8 @@ from watchlist_app.api.views import (
     WatchListDetailAV,
     StreamPlatformAV,
     StreamPlatformDetailAV,
+    ReviewList,
+    ReviewDetail,
 )
 
 urlpatterns = [
@@ -15,4 +17,6 @@ urlpatterns = [
     path("<int:pk>", WatchListDetailAV.as_view(), name="watchlist-detail"),
     path("stream/", StreamPlatformAV.as_view(), name="stream-platform"),
     path("stream/<int:pk>", StreamPlatformDetailAV.as_view(), name="stream-detail"),
+    path("review/", ReviewList.as_view(), name="review-list"),
+    path("review/<int:pk>", ReviewDetail.as_view(), name="review-detail"),
 ]
